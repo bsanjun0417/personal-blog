f74637976020fdf97fbb3a00d08956148b9063fa
백산 <69620341+bsanjun0417@users.noreply.github.com>
2026-05-20 15:30:08 +0900

    add3

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_BeksF from "./nanas-bread-04.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fprd%2Findex.md";
+export default new Map([["./nanas-bread-04.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fprd%2Findex.md", __ASTRO_IMAGE_IMPORT_BeksF]]);
+		
